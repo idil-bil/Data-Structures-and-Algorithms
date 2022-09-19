fa7ba2f62b73bdea9f736d56b4980c7b4b3a4a4c
idil-bil <89355539+idil-bil@users.noreply.github.com>
2022-09-19 01:34:57 -0700

    Update mazesolver.c

--- Lab4/mazesolver.c
+++ Lab4/mazesolver.c
@@ -2,8 +2,6 @@
  File:					mazesolver.c
  Purpose:				Mazesolver for Lab4 Take Home
  Author:			Idil Bil and Leif Moen
- Student #s:		21344189 and 97685127
- CWLs:      		y3c2z and kitmee10
  Date:				November 21,2021
  */
 
